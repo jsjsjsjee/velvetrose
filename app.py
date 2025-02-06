53b463cd154521c7334cd7eea3b0336bcc5a6552
jsjsjsjee <160294769+jsjsjsjee@users.noreply.github.com>
2025-02-06 10:09:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,5 +19,5 @@ def contact():
         return "<h1><center>Thank You For Your Submission!</center></h1>"
     return render_template("contact.html")
 
-if __name__=="__main"
+if __name__=="__main":
     app.run(debug=True)

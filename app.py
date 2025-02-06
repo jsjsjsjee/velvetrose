9cf3af987f78c6475f4fdcd9f42df3c6f0351932
Rushik Yadav <rushikyadav06@gmail.com>
2025-02-06 10:05:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template,request
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/rooms')
+def rooms():
+    return render_template("rooms.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact',methods=['POST','GET'])
+def contact():
+    if request.method == "POST":
+        return "<h1><center>Thank You For Your Submission!</center></h1>"
+    return render_template("contact.html")
+
+app.run(debug=True)
